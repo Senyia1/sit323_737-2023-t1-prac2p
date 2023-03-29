c305184abfdd853026d4ae375378bd52c7e837ec
Senyia1 <s221200347@deakin.edu.au>
2023-03-29 18:05:16 +0800

    Task4.JWT

--- Task4/app.js
+++ Task4/app.js
@@ -1,65 +1,81 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
-// Body parsing middleware
-app.use(express.json());
+// configure body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Addition endpoint
-app.post('/add', (req, res) => {
+// configure Passport.js
+const JWTStrategy = passportJWT.Strategy;
+const options = {
+  jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_secret_key_here'
+};
+
+passport.use(new JWTStrategy(options, (jwtPayload, done) => {
+  // find the user in the database if needed
+  if (jwtPayload.sub === 'Senyia@example.com') {
+    return done(null, true);
+  } else {
+    return done(null, false);
+  }
+}));
+
+app.use(passport.initialize());
+
+// define the routes
+app.get('/', (req, res) => {
+  res.send('Calculator Microservice');
+});
+
+app.post('/login', (req, res) => {
+  // validate the user's credentials
+  if (req.body.username === 'Senyia' && req.body.password === 'secret') {
+    // generate a JWT token
+    const token = jwt.sign({ sub: 'Senyia@example.com' }, 'your_secret_key_here');
+    // return the token to the client
+    res.json({ token: token });
+  } else {
+    // return an error response
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+});
+
+app.post('/add', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { num1, num2 } = req.body;
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).send({ error: 'Invalid input parameters' });
-    return;
-  }
-  const result = num1 + num2;
-  res.send({ result });
+  const result = Number(num1) + Number(num2);
+  res.json({ result: result });
 });
 
-// Subtraction endpoint
-app.post('/subtract', (req, res) => {
+app.post('/subtract', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { num1, num2 } = req.body;
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).send({ error: 'Invalid input parameters' });
-    return;
-  }
-  const result = num1 - num2;
-  res.send({ result });
+  const result = Number(num1) - Number(num2);
+  res.json({ result: result });
 });
 
-// Multiplication endpoint
-app.post('/multiply', (req, res) => {
+app.post('/multiply', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { num1, num2 } = req.body;
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).send({ error: 'Invalid input parameters' });
-    return;
-  }
-  const result = num1 * num2;
-  res.send({ result });
+  const result = Number(num1) * Number(num2);
+  res.json({ result: result });
 });
 
-// Division endpoint
-app.post('/divide', (req, res) => {
+app.post('/divide', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { num1, num2 } = req.body;
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).send({ error: 'Invalid input parameters' });
-    return;
+  if (Number(num2) === 0) {
+    res.status(400).json({ error: 'Cannot divide by zero' });
+  } else {
+    const result = Number(num1) / Number(num2);
+    res.json({ result: result });
   }
-  if (num2 === 0) {
-    res.status(400).send({ error: 'Cannot divide by zero' });
-    return;
-  }
-  const result = num1 / num2;
-  res.send({ result });
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send({ error: 'Something went wrong!' });
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-
-app.listen(port, () => {
-  console.log(`Calculator microservice listening at http://localhost:${port}`);
-});
-
